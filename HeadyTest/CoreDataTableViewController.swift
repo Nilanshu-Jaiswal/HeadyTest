c49756e1bbda5969036a7e4e71cc902181c69cf7
Nilanshu Jaiswal <nilanshu@Nilanshus-MacBook-Air.local>
2017-12-12 20:14:45 +0530

    Saving the Category data in the WelcomeViewController. Created CoreDataTableViewController to implement NSFetchedResultsController Delegate functions. Showing the saved data in the category TV controller.

---
+++ HeadyTest/CoreDataTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  CoreDataTableViewController.swift
+//  HeadyTest
+//
+//  Created by Nilanshu Jaiswal on 12/12/17.
+//  Copyright © 2017 Nilanshu Jaiswal. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "HOME", style: .plain, target: self, action: #selector(home))
+    }
+    
+    @objc func home() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
+    
+    var fetchedResultsController : NSFetchedResultsController<NSFetchRequestResult>? {
+        didSet {
+            fetchedResultsController?.delegate = self
+            executeSearch()
+            tableView.reloadData()
+        }
+    }
+    
+    init(fetchedResultsController fc : NSFetchedResultsController<NSFetchRequestResult>, style : UITableViewStyle = .plain) {
+        fetchedResultsController = fc
+        super.init(style: style)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+extension CoreDataTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if let fc = fetchedResultsController {
+            return (fc.sections?.count)!
+        } else {
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let fc = fetchedResultsController {
+            return fc.sections![section].numberOfObjects
+        } else {
+            return 0
+        }
+    }
+}
+
+extension CoreDataTableViewController {
+    func executeSearch() {
+        if let fc = fetchedResultsController {
+            do {
+                try fc.performFetch()
+            } catch let e as NSError {
+                print("Error while trying to perform a search: \n\(e)\n\(fetchedResultsController)")
+            }
+        }
+    }
+}
+
+extension CoreDataTableViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        let set = IndexSet(integer: sectionIndex)
+        switch (type) {
+        case .insert:
+            tableView.insertSections(set, with: .fade)
+        case .delete:
+            tableView.deleteSections(set, with: .fade)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch(type) {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .fade)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        }
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+
+}
--- HeadyTest/WelcomeViewController.swift
+++ HeadyTest/WelcomeViewController.swift
@@ -12,6 +12,9 @@ import SwiftyJSON
 
 class WelcomeViewController: UIViewController {
 
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getData()
@@ -25,13 +28,62 @@ class WelcomeViewController: UIViewController {
             case .success:
                 if let value = response.result.value {
                     let json = JSON(value)
-                    print(json)
+                    
+                    let jsonCategories = json["categories"]
+                    
+                    if jsonCategories.count > 0 {
+                        
+                        for category in 0...(jsonCategories.count - 1) {
+                            let cId = jsonCategories[category]["id"].intValue
+                            let cName = jsonCategories[category]["name"].stringValue
+                            let cProduct = jsonCategories[category]["products"]
+                            let cChild = jsonCategories[category]["child_categories"].stringValue
+                            
+                            let tempCategory = Categories(id1: cId, name: cName, childCategories: cChild, context: self.context!)
+                            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+                            
+                            if cProduct.count > 0 {
+                                for product in 0...(cProduct.count - 1) {
+                                    let pId = cProduct[product]["id"].intValue
+                                    let pName = cProduct[product]["name"].stringValue
+                                    let pDate = cProduct[product]["date_added"].stringValue
+                                    let pTaxName = cProduct[product]["tax"]["name"].stringValue
+                                    let pTaxValue = cProduct[product]["tax"]["value"].stringValue
+                                    let pVariants = cProduct[product]["variants"]
+                                    let pviewCount = 0
+                                    let porderCount = 0
+                                    let pshares = 0
+                                    
+                                    let productCD = Products(id2: pId, name: pName, dateAdded: pDate, taxName: pTaxName, taxValue: pTaxValue, viewCount: pviewCount, orderCount: porderCount, shares: pshares, context: self.context!)
+                                    productCD.category = tempCategory
+                                    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+                                    
+                                    if pVariants.count > 0 {
+                                        for variant in 0...(pVariants.count - 1) {
+                                            let vId = pVariants[variant]["id"].intValue
+                                            let vColor = pVariants[variant]["color"].stringValue
+                                            let vSize = pVariants[variant]["size"].stringValue
+                                            let vPrice = pVariants[variant]["price"].stringValue
+                                            
+                                            let variantCD = Variants(id3: vId, color: vColor, size: vSize, price: vPrice, context: self.context!)
+                                            variantCD.product = productCD
+                                            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        print("No Data Present")
+                    }
                 }
             case .failure(let error):
                 print(error)
             }
         }
     }
+
 
     @IBAction func startButton(_ sender: UIButton) {
         let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
