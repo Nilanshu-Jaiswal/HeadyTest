57de5ffecac76917adb406004f4299cbab6b318e
Nilanshu Jaiswal <nilanshu@Nilanshus-MacBook-Air.local>
2017-12-12 18:48:09 +0530

    Added getData() to get JSON data asynchronously. Added CategoryTableViewController and implemented navigation to it.

--- HeadyTest/WelcomeViewController.swift
+++ HeadyTest/WelcomeViewController.swift
@@ -14,12 +14,30 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
+    }
+    
+    func getData() {
+        let URL = "https://stark-spire-93433.herokuapp.com/json"
+        
+        Alamofire.request(URL, method: .get, parameters: nil, encoding: URLEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    print(json)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
-
-
     @IBAction func startButton(_ sender: UIButton) {
-        
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextViewController: CategoryTableViewController! = mainStoryboard.instantiateViewController(withIdentifier: "CategoryTableViewController") as! CategoryTableViewController
+        nextViewController.title = "Categories"
+        self.navigationController?.pushViewController(nextViewController, animated: true)
     }
     
 }
